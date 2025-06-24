1cfda54e7b433c0c718e48b2dbaf076b72e09282
Mauro Palavecino <mauropalavecino65@gmail.com>
2025-06-23 22:49:47 -0300

    Test puesto, categoria

--- tests/test_laboral.py
+++ tests/test_laboral.py
@@ -2,6 +2,7 @@ import pytest
 from crud.crudAdmintrador import AdminCRUD
 from crud.database import db
 
+
 def obtener_id_departamento(nombre_departamento: str):
     conn = db.get_connection()
     try:
@@ -12,15 +13,39 @@ def obtener_id_departamento(nombre_departamento: str):
                 FROM departamento
                 WHERE nombre = %s
             """,
-                (nombre_departamento,),
+                (nombre_departamento),
             )
             resultado = cur.fetchone()
         if resultado is None:
-            raise ValueError(f"Departamento con nombre {nombre_departamento} no encontrado")
-        
+            raise ValueError(
+                f"Departamento con nombre {nombre_departamento} no encontrado"
+            )
+
         return resultado
     finally:
         db.return_connection(conn)
+
+
+def obtener_id_puesto(nombre_puesto: str):
+    conn = db.get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT id_puesto
+                FROM puesto
+                WHERE nombre = %s
+            """,
+                (nombre_puesto),
+            )
+            resultado = cur.fetchone()
+        if resultado is None:
+            raise ValueError(f"Puesto con nombre {nombre_puesto} no encontrado")
+
+        return resultado
+    finally:
+        db.return_connection(conn)
+
 
 @pytest.mark.usefixtures("setup_schema")
 class TestDepartamento:
@@ -76,7 +101,7 @@ class TestDepartamento:
         nombre_departamento = "Marketing"
         desc_departamento = "Diseña e implementa estrategias de promoción."
         AdminCRUD.agregar_departamento(nombre_departamento, desc_departamento)
-        
+
         id_departamento = obtener_id_departamento(nombre_departamento)
         AdminCRUD.eliminar_departamento(id_departamento)
 
@@ -84,6 +109,7 @@ class TestDepartamento:
         id_departamento = 1
         with pytest.raises(Exception):
             AdminCRUD.eliminar_departamento(id_departamento)
+
 
 @pytest.mark.usefixtures("setup_schema")
 class TestPuesto:
@@ -104,37 +130,79 @@ class TestPuesto:
             AdminCRUD.agregar_puesto(nombre_puesto)
 
     def test_listar_puestos_exitoso(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        puestos = [
+            "Analista de Datos",
+            "Analista de Recursos Humanos",
+            "Diseñador UX/UI",
+        ]
+        for puesto in puestos:
+            AdminCRUD.agregar_puesto(puesto)
+
+        resultado = AdminCRUD.listar_puestos()
+        nombres = [puesto["nombre"] for puesto in resultado]
+
+        assert len(resultado) == 3
+        assert "Analista de Datos" in nombres
+        assert "Analista de Recursos Humanos" in nombres
+        assert "Diseñador UX/UI" in nombres
+
     def test_listar_puestos_sin_puestos(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        resultado = AdminCRUD.listar_puestos()
+        assert len(resultado) == 0
+
     def test_eliminar_puesto_exitoso(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        nombre_puesto = "Analista de Datos"
+        AdminCRUD.agregar_puesto(nombre_puesto)
+        id_puesto = obtener_id_puesto(nombre_puesto)
+        AdminCRUD.eliminar_puesto(id_puesto)
+
     def test_eliminar_puesto_inexistente(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        with pytest.raises(Exception):
+            AdminCRUD.eliminar_puesto(1)
+
+
 @pytest.mark.usefixtures("setup_schema")
 class TestCategoria:
 
     def test_agregar_categoria_exitoso(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        nombre_categoria = "Senior"
+        AdminCRUD.agregar_categoria(nombre_categoria)
+
     def test_agregar_categoria_existente(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        nombre_categoria = "Senior"
+        AdminCRUD.agregar_categoria(nombre_categoria)
+        with pytest.raises(Exception):
+            AdminCRUD.agregar_categoria(nombre_categoria)
+
     def test_agregar_categoria_vacia(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        nombre_categoria = ""
+        with pytest.raises(Exception):
+            AdminCRUD.agregar_categoria(nombre_categoria)
+
     def test_listar_categorias_exitoso(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        categorias = ["Trainee", "Junior", "Senior"]
+        for categoria in categorias:
+            AdminCRUD.agregar_categoria(categoria)
+
+        resultado = AdminCRUD.listar_categorias()
+        nombres = [categoria["nombre_categoria"] for categoria in resultado]
+
+        assert len(resultado) == 3
+        assert "Trainee" in nombres
+        assert "Junior" in nombres
+        assert "Senior" in nombres
+
     def test_listar_categorias_sin_categorias(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        resultado = AdminCRUD.listar_categorias()
+        assert len(resultado) == 0
+
     def test_eliminar_categoria_exitoso(self):
-        raise NotImplementedError("Test sin terminar")
-    
+        nombre_categoria = "Senior"
+        AdminCRUD.agregar_categoria(nombre_categoria)
+
+        id_categoria = 1
+        AdminCRUD.eliminar_categoria(id_categoria)
+
     def test_eliminar_categoria_inexistente(self):
-        raise NotImplementedError("Test sin terminar")
+        with pytest.raises(Exception):
+            AdminCRUD.eliminar_categoria(1)
